640ae8b940b688cbc339a13cc3755e382427aa69
giorgitrapaidze <gio.trapaidze@gmail.com>
2021-11-22 12:02:09 +0000

    added setAuthFlag

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,25 +1,27 @@
 const router = require("express").Router();
 const isLoggedIn = require("./../middleware/isLoggedIn");
+const setAuthFlag = require("./../middleware/setAuthFlag")
+
 
 /* GET home page */
-router.get("/", (req, res, next) => {
+router.get("/", setAuthFlag, (req, res, next) => {
+  let data
   // Check if the incoming request has a valid cookie/session
   let userIsLoggedIn = false;
-  if (req.session.user) {
-    userIsLoggedIn = true;
-    data = {
-      userIsLoggedIn: userIsLoggedIn,
-      userId: req.session.user._id
-    }
-  }else{
-    data = {
-      userIsLoggedIn: userIsLoggedIn,
-    }
-  }
+  // if (req.session.user) {
+  //   userIsLoggedIn = true;
+  //   data = {
+  //     userIsLoggedIn: userIsLoggedIn,
+  //     userId: req.session.user._id
+  //   }
+  // }else{
+  //   data = {
+  //     userIsLoggedIn: userIsLoggedIn,
+  //   }
+  // }
 
-  
 
-  res.render("index", data);
+  res.render("index", {user: req.user} );
 });
 
 // GET /secret
